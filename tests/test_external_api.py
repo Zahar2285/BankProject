8bce93860bc2d77c3b9f2053413460c4485a0d07
Alexandr <Zahar2285@mail.ru>
2026-07-21 21:32:23 +0300

    Add test_external_api

---
+++ tests/test_external_api.py
@@ -0,0 +1,22 @@
+from unittest.mock import patch
+
+from src.external_api import convert_to_rub
+
+
+@patch("src.external_api.requests.get")
+def test_convert_usd(mock_get):
+
+    mock_get.return_value.json.return_value = {
+        "result": 9100
+    }
+
+    transaction = {
+        "operationAmount": {
+            "amount": "100",
+            "currency": {
+                "code": "USD"
+            }
+        }
+    }
+
+    assert convert_to_rub(transaction) == 9100
