6f437b4266b06fe55905a52a3377d05881189ccc
Alexandr <Zahar2285@mail.ru>
2026-09-04 00:55:47 +0300

    add_test_axternal.py

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -3,8 +3,9 @@ from unittest.mock import patch
 from src.external_api import convert_to_rub
 
 
+@patch("src.external_api.os.getenv", return_value="test_api_key")
 @patch("src.external_api.requests.get")
-def test_convert_usd(mock_get):
+def test_convert_usd(mock_get, mock_getenv):
     mock_get.return_value.json.return_value = {
         "rates": {
             "RUB": 91.0
@@ -21,3 +22,4 @@ def test_convert_usd(mock_get):
     }
 
     assert convert_to_rub(transaction) == 9100.0
+    
