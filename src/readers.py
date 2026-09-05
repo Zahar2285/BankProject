b12b4bd9bfa70637f74e5b860809a8616a094dda
Alexandr <Zahar2285@mail.ru>
2026-09-05 12:09:47 +0300

    edit_readers.py

--- src/readers.py
+++ src/readers.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 
-def read_csv_file(file_path: str) -> list[dict]:
+def read_csv(file_path: str) -> list[dict]:
     """Считывает транзакции из CSV-файла и возвращает список словарей."""
     dataframe = pd.read_csv(file_path, sep=";")
     return dataframe.to_dict(orient="records")
+
 
 def read_excel(path: str) -> list[dict]:
     """
